83543e58fddfa26662e0b8b14050165b230720cb
luthfi <alfarisi.luthfi@yahoo.com>
2023-10-30 16:37:09 +0700

    add convert image to hex

--- src/main/java/com/ConvertHexAndImageApplication.java
+++ src/main/java/com/ConvertHexAndImageApplication.java
@@ -11,7 +11,7 @@ import java.nio.file.Files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletComponentScan; 
 
 @SpringBootApplication
 public class ConvertHexAndImageApplication {
--- src/main/java/com/api/ConvertController.java
+++ src/main/java/com/api/ConvertController.java
@@ -1,11 +1,16 @@
 package com.api;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.util.Map;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,13 +18,27 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api")
 public class ConvertController {
 
-    @PostMapping(value = "/convertToHex")
-    public String convert(@RequestBody Map<String,Object> file){
+    @PostMapping(value = "/convertToHex", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String convert(@RequestPart(name = "file") MultipartFile file){
+        String a = "";
+        try {
+            byte [] byteArr=file.getBytes();
+            a = getHexString(byteArr);        
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
 
-        System.out.println(file.get(0));
-
-        return "test";
+        return a;
 
     }
+
+    public static String getHexString(byte[] b) throws Exception {
+		String result = "";
+		for (int i=0; i < b.length; i++) {
+		  result +=
+				Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+		}
+		return result;
+	  }
     
 }
